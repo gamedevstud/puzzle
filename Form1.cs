45d75a1c46906c46e6f8b196b6959e72b73f0ec7
parenyukp <accountsgames@yandex.ru>
2016-02-15 11:28:59 +0300

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,1 +1,28 @@
-//запускает алгоритм работы с изображением
+public Form1()
+        {
+            InitializeComponent(); // InitializeComponent - процедура создающая видимую форму и связывающая все события с имеющимися  процедурами
+
+
+        }
+
+        private Form3 f;
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {    
+        } //кнопка начать игру
+
+    
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+        }//кнопка выхода
+
+        private void pictureBox1_Click(object sender, EventArgs e)//добавляет на форму элемент picturebox
+
+        {
+
+        }
+    }
+}
