cd059abba6228c38afb6da0dacee263c10064c87
parenyukp <accountsgames@yandex.ru>
2016-02-15 11:28:24 +0300

    Update ResizingImage.cs

--- ResizingImage.cs
+++ ResizingImage.cs
@@ -1,1 +1,14 @@
 //масштабирует изображение .
+
+public class ResizingImage
+{
+public Image ResizeImg(Image b, int nWidth, int nHeight){
+  
+  //Int nWidth - ширина будущего изображения
+//int nHeight - высота будущего изображения
+//Результатом работы метода будет изображение размером 400х400 пикселей готовое к нарезке.
+} 
+}
+
+
+
