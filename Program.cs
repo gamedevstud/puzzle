e0178ac33d48ced628480cc578b80fadcd3e7a1e
parenyukp <accountsgames@yandex.ru>
2016-02-15 11:30:57 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
 //запускает класс Form1 и выводит его на экран ,т.е запускает пользовательский интерфейс.
+static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.Run(new Form1());
+        }
