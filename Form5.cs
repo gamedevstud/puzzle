965e0cfc8fbd93cb919d6783a0fa99960ed07785
parenyukp <accountsgames@yandex.ru>
2016-02-15 11:30:39 +0300

    Create Form5.cs

---
+++ Form5.cs
@@ -0,0 +1,32 @@
+public partial class Form5 : Form
+    {
+        public Form5(bool is_winner, int minets, int seconds, int col_vo)
+        {
+            //конструктор класса
+        }
+
+
+        private bool is_winner = false;//проверка на победу
+        private int minets;//начальное время минуты
+        private int seconds;//начальное время секунды
+        
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+           //отображение минут на форме
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+           //отображение секунд на форме
+        }
+
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+          //конечный расчет остатка времени
+        }
+
+
+        
+    } 
