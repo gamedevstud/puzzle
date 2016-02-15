ac616f7d48ba469771d2f9c147f67c1a6ec9d3a6
parenyukp <accountsgames@yandex.ru>
2016-02-15 11:30:05 +0300

    Update Form3.cs

--- Form3.cs
+++ Form3.cs
@@ -1,1 +1,37 @@
-//определяет уровень сложности игры.
+public class Form3 : Form
+    {
+        {
+
+            radioButton1.MouseClick += radioButton1_MouseClick;
+            label1.MouseClick += radioButton1_MouseClick;
+            radioButton2.MouseClick += radioButton2_MouseClick;
+            label2.MouseClick += radioButton2_MouseClick;
+            radioButton3.MouseClick += radioButton3_MouseClick;
+            label3.MouseClick += radioButton3_MouseClick;
+//привязка к элементам формы имеющихся обработчиков нажатия на них.
+        }
+
+        private int col_vo;//определяется  поле для расчета количества пазлов.
+
+        void radioButton3_MouseClick(object sender, MouseEventArgs e)
+        {
+//обработчик нажатия кнопки выбора режима игры - легкий
+        }
+
+        void radioButton2_MouseClick(object sender, MouseEventArgs e)
+        {
+//обработчик нажатия кнопки выбора режима игры - средний        }    
+
+        void radioButton1_MouseClick(object sender, MouseEventArgs e)
+        {  
+//обработчик нажатия кнопки выбора режима игры - сложный}
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //обработчик нажатия кнопки старт
+           
+        }
+
+}
